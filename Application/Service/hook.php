bf51d88027d86fbdde79432c67ac42cf49cde437
AlexAgapitov <agapitovafoj@gmail.com>
2025-01-16 23:24:24 +0300

    K

--- Application/Service/hook.php
+++ Application/Service/hook.php
@@ -9,7 +9,7 @@ ini_set('ignore_repeated_errors', TRUE); // always use TRUE
 ini_set('display_errors', FALSE); // Error/Exception display, use FALSE only in production environment or real server. Use TRUE in development environment
 
 ini_set('log_errors', TRUE); // Error/Exception file logging engine.
-ini_set('error_log', './errors.log'); // Logging file path
+ini_set('error_log', __DIR__.'/errors.log'); // Logging file path
 
 $APPLICATION_CONFIG = include_once __DIR__ . '/../Core/config.php';
 
