4d71d2a8cb0d803b93888e38077b4d67a5c4f719
AlexAgapitov <agapitovafoj@gmail.com>
2025-01-16 22:47:56 +0300

    K

--- Application/Service/hook.php
+++ Application/Service/hook.php
@@ -1,1 +1,20 @@
 <?php
+// Load composer
+require __DIR__ . '/../../vendor/autoload.php';
+
+$APPLICATION_CONFIG = include_once __DIR__ . '/../Core/config.php';
+
+$bot_api_key  = $APPLICATION_CONFIG['bot_api_key'];
+$bot_username  = $APPLICATION_CONFIG['bot_username'];
+
+try {
+    // Create Telegram API object
+    $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
+
+    // Handle telegram webhook request
+    $telegram->handle();
+} catch (Longman\TelegramBot\Exception\TelegramException $e) {
+    // Silence is golden!
+    // log telegram errors
+    // echo $e->getMessage();
+}
