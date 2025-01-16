097b322331adaf8f6821b849991de10cbb452d4d
AlexAgapitov <agapitovafoj@gmail.com>
2025-01-16 23:23:21 +0300

    K

--- Application/Service/hook.php
+++ Application/Service/hook.php
@@ -2,6 +2,15 @@
 // Load composer
 require __DIR__ . '/../../vendor/autoload.php';
 
+error_reporting(E_ALL); // Error/Exception engine, always use E_ALL
+
+ini_set('ignore_repeated_errors', TRUE); // always use TRUE
+
+ini_set('display_errors', FALSE); // Error/Exception display, use FALSE only in production environment or real server. Use TRUE in development environment
+
+ini_set('log_errors', TRUE); // Error/Exception file logging engine.
+ini_set('error_log', './errors.log'); // Logging file path
+
 $APPLICATION_CONFIG = include_once __DIR__ . '/../Core/config.php';
 
 $bot_api_key  = $APPLICATION_CONFIG['bot_api_key'];
