b825f63a30e18f96a890554dd0a0508390852104
AlexAgapitov <agapitovafoj@gmail.com>
2025-01-16 22:44:56 +0300

    f

---
+++ Application/Core/config.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'bot_api_key' => '8083746843:AAE10syeqtx0H45K9dlsgClHiktolAZmoTo',
+    'bot_username' => 'dont_forget_pay_domen_bot',
+];
---
+++ Service/set.php
@@ -0,0 +1,23 @@
+<?php
+// Load composer
+require __DIR__ . '/../vendor/autoload.php';
+
+$APPLICATION_CONFIG = include_once __DIR__ . '/../Application/Core/config.php';
+
+$bot_api_key  = $APPLICATION_CONFIG['bot_api_key'];
+$bot_username  = $APPLICATION_CONFIG['bot_username'];
+$hook_url     = 'https://aga-tg-bots.ru/Service/hook.php';
+
+try {
+    // Create Telegram API object
+    $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
+
+    // Set webhook
+    $result = $telegram->setWebhook($hook_url);
+    if ($result->isOk()) {
+        echo $result->getDescription();
+    }
+} catch (Longman\TelegramBot\Exception\TelegramException $e) {
+    // log telegram errors
+    // echo $e->getMessage();
+}
